2a8f21e98108a29e17e40da2d86d834e414998ca
sufjan bajric <sufjan.ba@gmail.com>
2025-09-25 09:45:59 +0000

    import { genkitNext } from '@genkit-ai/next';

--- src/app/api/genkit/[...flow]/route.ts
+++ src/app/api/genkit/[...flow]/route.ts
@@ -1,4 +1,11 @@
 import {genkitNext} from '@genkit-ai/next';
-import '@/ai/dev';
+import '@/ai/flows/generate-text-from-prompt';
+import '@/ai/flows/suggest-improvements';
+import '@/ai/flows/verify-source-facts';
+import '@/ai/flows/creative-brainstorming';
+import '@/ai/flows/rewrite-text';
+import '@/ai/flows/continue-writing';
+import '@/ai/flows/make-longer';
+import '@/ai/flows/make-shorter';
 
 export const {POST} = genkitNext();
